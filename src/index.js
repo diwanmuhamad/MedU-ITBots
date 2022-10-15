46f9d3c65cd26a7c088369a73fab89543fe523e9
Diwan <diwan.muhamad@nawadata.id>
2022-10-15 21:11:15 +0700

    register and login page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SignIn from './page/login';
+import SignUp from './page/register';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<SignIn></SignIn>}></Route>
+        <Route path="/register" element={<SignUp></SignUp>}></Route>
+      </Routes>
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
